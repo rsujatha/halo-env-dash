5c4bea364f696456fab51ec8ff9a0b5483f84e4b
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2021-03-07 23:56:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ df0 = pd.read_pickle('./data/df0')
 
 
 colr = ["red","blue", "#17BECF","teal"]
-#colrr = ["red", "red","blue","blue", "#17BECF","#17BECF","teal","teal"]
-#lnstyle = ["solid","dot","solid","dot","solid","dot","solid","dot"]
-#env = ['delta0000-','delta0000-','alpha0001-','alpha0001-','alpha0p30-','alpha0p30-','alpha0p55-','alpha0p55-']
-#scales=['2r','3r','4r','5r','6r','7r','8r','9r','10r']
-#scaleconst = np.array([0.5,1.25,1.5,2.5,5,10])
+colrr = ["red", "red","blue","blue", "#17BECF","#17BECF","teal","teal"]
+lnstyle = ["solid","dot","solid","dot","solid","dot","solid","dot"]
+env = ['delta0000-','delta0000-','alpha0001-','alpha0001-','alpha0p30-','alpha0p30-','alpha0p55-','alpha0p55-']
+scales=['2r','3r','4r','5r','6r','7r','8r','9r','10r']
+scaleconst = np.array([0.5,1.25,1.5,2.5,5,10])
 
     
 #col_options = [dict(label=x,value=x) for x in df['MassRange'].unique()]
