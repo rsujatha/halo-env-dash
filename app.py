35acd55e29808e61456fd9d242a17315d41a002b
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2021-03-07 23:54:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+import numpy as np
 
 df_corrcon = pd.read_pickle('./data/df_corrcon')
 df_corrvar = pd.read_pickle('./data/df_corrvar')
@@ -11,12 +12,12 @@ df = pd.read_pickle('./data/df_scl')
 df0 = pd.read_pickle('./data/df0')
 
 
-colr = ["red","blue", "#17BECF","teal"]
-colrr = ["red", "red","blue","blue", "#17BECF","#17BECF","teal","teal"]
-lnstyle = ["solid","dot","solid","dot","solid","dot","solid","dot"]
-env = ['delta0000-','delta0000-','alpha0001-','alpha0001-','alpha0p30-','alpha0p30-','alpha0p55-','alpha0p55-']
-scales=['2r','3r','4r','5r','6r','7r','8r','9r','10r']
-scaleconst = np.array([0.5,1.25,1.5,2.5,5,10])
+#colr = ["red","blue", "#17BECF","teal"]
+#colrr = ["red", "red","blue","blue", "#17BECF","#17BECF","teal","teal"]
+#lnstyle = ["solid","dot","solid","dot","solid","dot","solid","dot"]
+#env = ['delta0000-','delta0000-','alpha0001-','alpha0001-','alpha0p30-','alpha0p30-','alpha0p55-','alpha0p55-']
+#scales=['2r','3r','4r','5r','6r','7r','8r','9r','10r']
+#scaleconst = np.array([0.5,1.25,1.5,2.5,5,10])
 
     
 #col_options = [dict(label=x,value=x) for x in df['MassRange'].unique()]
