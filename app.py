acc49c0baea442ceb4d6a757d2c5362c84ff4789
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2021-03-07 22:00:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
-
+from dash.dependencies import Input,Output
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 colr = ["red","blue", "#17BECF","teal"]
 colrr = ["red", "red","blue","blue", "#17BECF","#17BECF","teal","teal"]
