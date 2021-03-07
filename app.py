9191162a08a12609292db1ae80e5b22830c01cad
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2021-03-07 21:59:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 
+
+colr = ["red","blue", "#17BECF","teal"]
+colrr = ["red", "red","blue","blue", "#17BECF","#17BECF","teal","teal"]
+lnstyle = ["solid","dot","solid","dot","solid","dot","solid","dot"]
+env = ['delta0000-','delta0000-','alpha0001-','alpha0001-','alpha0p30-','alpha0p30-','alpha0p55-','alpha0p55-']
+scales=['2r','3r','4r','5r','6r','7r','8r','9r','10r']
+scaleconst = np.array([0.5,1.25,1.5,2.5,5,10])
+
+    
+col_options = [dict(label=x,value=x) for x in df['MassRange'].unique()]
+col_options_var =  [dict(label=x,value=x) for x in df['HaloProperty'].unique()]
+
 ########### Define your variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
